9f16ddeb434d5ab5c4ff5cbeb7895b1fdeb68654
bosira <sbobfitz2@gmail.com>
2023-12-11 16:30:20 +0000

    removed unwanted definition

--- automation/SetUpUtilities.psm1
+++ automation/SetUpUtilities.psm1
@@ -1,27 +1,5 @@
 $envPathRegKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 
-function Get-HyperV {
-    $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
-    # Check if Hyper-V is enabled
-    if($hyperv.State -eq "Enabled") {
-        Write-Host "Hyper-V is enabled."
-    } else {
-        Write-Host "Hyper-V is disabled."
-    }
-    
-}
-
-function Set-VmSwitch {    
-    $net = Get-NetAdapter | Where-Object {  $_.Status -eq 'Up' }
-    New-VMSwitch -Name "External VM Switch" -AllowManagementOS $True -NetAdapterName $net.Name
-}
-
-function Get-VmSwitch {
-    $SwitchName = "External VM Switch"
-    return $SwitchName
-    
-}
-
 function Get-LatestToolVersion($repository) {
     try {
         $uri = "https://api.github.com/repos/$repository/releases/latest"
