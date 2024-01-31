7ae051c9ce544f750e893489de73b437ea5ebaff
Bob Sira <bosira@microsoft.com>
2024-01-31 00:41:14 -0800

    setup file to test the configuration

---
+++ automation/SetUp.ps1
@@ -0,0 +1,17 @@
+$SwitchName = "External VM Switch" 
+$ISOFile = "C:\Users\bosira\Downloads\SERVER_EVAL_x64FRE_en-us-uni.iso"
+$VMName = 'minikube-m05' 
+$VM = @{
+    Name = $VMName;
+    MemoryStartupBytes = 1GB;
+    NewVHDPath = "${env:homepath}\.minikube\machines\$VMName\VHD.vhdx";
+    NewVHDSizeBytes = 10GB;
+    BootDevice = "VHD";
+    Path = "${env:homepath}\.minikube\machines\";
+    SwitchName = $SwitchName
+}
+New-VM @VM
+Set-VM -Name $VMName -ProcessorCount 2 -AutomaticCheckpointsEnabled $false
+Set-VMProcessor -VMName $VMName -ExposeVirtualizationExtensions $true
+Set-VMDvdDrive -VMName $VMName -Path $ISOFile
+Start-VM -Name $VMName
